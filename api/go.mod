46cf46374e35f125b7d544aa7e65bf3be4391617
shida22553 <53365248+shida22553@users.noreply.github.com>
2021-04-17 20:53:13 +0900

    using firebase on server side

--- api/go.mod
+++ api/go.mod
@@ -3,9 +3,13 @@ module shida22552/my-scrapbook
 go 1.16
 
 require (
+	cloud.google.com/go/firestore v1.5.0 // indirect
+	cloud.google.com/go/storage v1.14.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.7.1
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	golang.org/x/oauth2 v0.0.0-20210413134643-5e61552d6c78 // indirect
+	google.golang.org/api v0.44.0 // indirect
 	gorm.io/driver/mysql v1.0.5
 	gorm.io/gorm v1.21.7
 )
--- api/main.go
+++ api/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 
+	firebase "firebase.google.com/go"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/api/option"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -55,8 +61,40 @@ func getGormConnect() *gorm.DB {
 	return db
 }
 
+func authMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Firebase SDK のセットアップ
+		opt := option.WithCredentialsFile("/myapp/my-scrapbook-dev-9f06fe374e05.json")
+		app, err := firebase.NewApp(context.Background(), nil, opt)
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			os.Exit(1)
+		}
+		auth, err := app.Auth(context.Background())
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			os.Exit(1)
+		}
+
+		// クライアントから送られてきた JWT 取得
+		authHeader := c.Request.Header.Get("Authorization")
+		idToken := strings.Replace(authHeader, "Bearer ", "", 1)
+
+		// JWT の検証
+		token, err := auth.VerifyIDToken(context.Background(), idToken)
+		if err != nil {
+			// JWT が無効なら Handler に進まず別処理
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			c.Abort()
+			return
+		}
+		log.Printf("Verified ID token: %v\n", token)
+	}
+}
+
 func main() {
 	r := gin.Default()
+	r.Use(authMiddleware())
 	db := getGormConnect()
 	db.AutoMigrate(&User{})
 	closeDb, err := db.DB()
@@ -66,15 +104,16 @@ func main() {
 	}
 	defer closeDb.Close()
 
-	// user := User{
-	// 	UId:  "EDqv7BAIG7XpGBaCU1YKCUUx3WP2",
-	// 	Name: "test",
-	// }
-	// insertUser(&user)
-
 	r.GET("/users", func(c *gin.Context) {
 		users := findAllUser()
 		c.JSON(http.StatusOK, users)
+	})
+	r.POST("/users", func(c *gin.Context) {
+		user := User{
+			Name: "test",
+		}
+		insertUser(&user)
+		c.JSON(http.StatusOK, user)
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
