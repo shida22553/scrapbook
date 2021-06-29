3d999939fbb6361ecf25f19da6c90616327940c8
shida22553 <53365248+shida22553@users.noreply.github.com>
2021-06-29 13:24:10 +0900

    apply firebase auth middleware

--- docker/api/infrastructure/router.go
+++ docker/api/infrastructure/router.go
@@ -1,7 +1,17 @@
 package infrastructure
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	firebase "firebase.google.com/go"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/api/option"
 	"myapp/interfaces/controllers"
 )
 
@@ -9,6 +19,11 @@ var Router *gin.Engine
 
 func init() {
 	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowHeaders = []string{"Authorization", "Content-Type"}
+	router.Use(cors.New(config))
+	router.Use(authMiddleware())
 
 	userController := controllers.NewUserController(NewSqlHandler())
 
@@ -17,4 +32,37 @@ func init() {
 	router.GET("/users/:id", func(c *gin.Context) { userController.Show(c) })
 
 	Router = router
+}
+
+func authMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Firebase SDK のセットアップ
+		opt := option.WithCredentialsFile("my-scrapbook-dev-9f06fe374e05.json")
+		app, err := firebase.NewApp(context.Background(), nil, opt)
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			os.Exit(1)
+		}
+		auth, err := app.Auth(context.Background())
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			os.Exit(1)
+		}
+
+		// クライアントから送られてきた JWT 取得
+		authHeader := c.Request.Header.Get("Authorization")
+		idToken := strings.Replace(authHeader, "Bearer ", "", 1)
+
+		// JWT の検証
+		token, err := auth.VerifyIDToken(context.Background(), idToken)
+		if err != nil {
+			// JWT が無効なら Handler に進まず別処理
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			c.Abort()
+			return
+		}
+		log.Printf("Verified ID token: %v\n", token)
+		c.Set("uid", token.UID)
+		c.Next()
+	}
 }
